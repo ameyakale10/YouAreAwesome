769b149e8d336b07e7e7a0555a02ba5e4db4fa3a
ameyakale10 <ameya.kale1010@gmail.com>
2024-08-13 10:49:00 -0400

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Ameya Kale on 2024-08-13.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
